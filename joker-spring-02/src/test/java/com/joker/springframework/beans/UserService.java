6b17fcc1d1410e6e82cb1e635198c358b8a3124d
joker <joker_monster@foxmail.com>
2022-09-21 17:36:46 +0800

    feat: init-joker-spring-02

---
+++ joker-spring-02/src/test/java/com/joker/springframework/beans/UserService.java
@@ -0,0 +1,17 @@
+package com.joker.springframework.beans;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author jokerzzccc
+ * @date 2022/9/17
+ */
+public class UserService {
+
+    public void queryUserInfo(){
+        System.out.println("Query User");
+    }
+
+}
