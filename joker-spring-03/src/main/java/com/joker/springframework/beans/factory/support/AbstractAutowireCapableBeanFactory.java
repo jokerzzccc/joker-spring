3750af80e97bd312d09abe26a6828fc93ed77e48
joker <joker_monster@foxmail.com>
2022-10-02 00:39:03 +0800

    fix: joker-spring-03 格式化

--- joker-spring-03/src/main/java/com/joker/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ joker-spring-03/src/main/java/com/joker/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -15,26 +15,26 @@ import java.lang.reflect.Constructor;
  */
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
 
-    private InstantiationStrategy instantiationStrategy  = new CglibSubclassingInstantiationStrategy();
+    private InstantiationStrategy instantiationStrategy = new CglibSubclassingInstantiationStrategy();
 
     @Override
     protected Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args) {
         Object bean = null;
         try {
             bean = createBeanInstance(beanDefinition, beanName, args);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
         addSingleton(beanName, bean);
         return bean;
     }
 
-    protected Object createBeanInstance( BeanDefinition beanDefinition, String beanName, Object[] args){
+    protected Object createBeanInstance(BeanDefinition beanDefinition, String beanName, Object[] args) {
         Constructor constructorToUse = null;
         Class<?> beanClass = beanDefinition.getBeanClass();
-         Constructor<?>[] declaredConstructors = beanClass.getDeclaredConstructors();
+        Constructor<?>[] declaredConstructors = beanClass.getDeclaredConstructors();
         for (Constructor ctor : declaredConstructors) {
-            if (null != args && ctor.getParameterTypes().length == args.length){
+            if (null != args && ctor.getParameterTypes().length == args.length) {
                 constructorToUse = ctor;
                 break;
             }
@@ -42,7 +42,7 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         return getInstantiationStrategy().instantiate(beanDefinition, beanName, constructorToUse, args);
     }
 
-    public InstantiationStrategy getInstantiationStrategy(){
+    public InstantiationStrategy getInstantiationStrategy() {
         return instantiationStrategy;
     }
 
